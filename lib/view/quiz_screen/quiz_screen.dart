fd3c3757ffb2bdde8af719f32407d0549436753d
Shihab k s <neethu@luminartechnolab.com>
2024-11-15 17:13:56 +0530

    quiz funcitonality completed

--- lib/view/quiz_screen/quiz_screen.dart
+++ lib/view/quiz_screen/quiz_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:quiz_app_july/dummy_db.dart';
 import 'package:quiz_app_july/utils/color_constants.dart';
-import 'package:quiz_app_july/view/quiz_screen/results_screen/results_screen.dart';
+import 'package:quiz_app_july/view/results_screen/results_screen.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({super.key});
@@ -14,6 +16,7 @@ class _QuizScreenState extends State<QuizScreen> {
   int currentQuestionIndex = 0;
 
   int? selectedAnsIndex;
+  int rightAnsCount = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,17 +55,22 @@ class _QuizScreenState extends State<QuizScreen> {
                   padding: const EdgeInsets.only(bottom: 15),
                   child: InkWell(
                     onTap: () {
-                      selectedAnsIndex = optionIndex;
-                      setState(() {});
+                      if (selectedAnsIndex == null) {
+                        selectedAnsIndex = optionIndex;
+                        if (selectedAnsIndex ==
+                            DummyDb.questions[currentQuestionIndex]
+                                ["answerIndex"]) {
+                          rightAnsCount++;
+                          log("right ans count = $rightAnsCount");
+                        }
+                        setState(() {});
+                      }
                     },
                     child: Container(
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(15),
-                          color: selectedAnsIndex == optionIndex
-                              ? ColorConstants.red
-                              : ColorConstants.darkBlue,
-                          border: Border.all(color: ColorConstants.darkBlue)),
+                          color: getColor(optionIndex)),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
@@ -87,42 +95,74 @@ class _QuizScreenState extends State<QuizScreen> {
                 ),
               ),
             ),
-            InkWell(
-              onTap: () {
-                if (currentQuestionIndex < DummyDb.questions.length - 1) {
-                  currentQuestionIndex++;
-                  setState(() {});
-                } else {
-                  // navigate to results screen
-                  print("results screen");
+            if (selectedAnsIndex != null)
+              InkWell(
+                onTap: () {
+                  if (currentQuestionIndex < DummyDb.questions.length - 1) {
+                    currentQuestionIndex++;
+                    selectedAnsIndex = null;
+                    setState(() {});
+                  } else {
+                    // navigate to results screen
+                    print("results screen");
 
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ResultsScreen(),
-                      ));
-                }
-              },
-              child: Container(
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: ColorConstants.warmBrown,
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ResultsScreen(
+                            rightAnsCount: rightAnsCount,
+                          ),
+                        ));
+                  }
+                },
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: ColorConstants.warmBrown,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text("Next",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: ColorConstants.darkBlue,
+                      )),
                 ),
-                alignment: Alignment.center,
-                child: Text("Next",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: ColorConstants.darkBlue,
-                    )),
               ),
-            ),
             SizedBox(height: 20)
           ],
         ),
       ),
     );
+  }
+
+// to set color for option seciton based on selected answer
+  Color getColor(int optionIndex) {
+    // to check whether an answer is selected or not
+    if (selectedAnsIndex != null) {
+      //to check whether the option is right or not
+      if (DummyDb.questions[currentQuestionIndex]["answerIndex"] ==
+          optionIndex) {
+        return Colors.green;
+      }
+    }
+
+//to check whether to the selected ansewer is equal to builder index
+    if (selectedAnsIndex == optionIndex) {
+      //to check whether the  selected answer is right or not
+      if (selectedAnsIndex ==
+          DummyDb.questions[currentQuestionIndex]["answerIndex"]) {
+        // if answer is right (green color)
+        return Colors.green;
+      } else {
+        // if answer is wrong (red color)
+        return ColorConstants.red;
+      }
+    } else {
+      // default return  blue colors
+      return ColorConstants.darkBlue;
+    }
   }
 }
--- lib/view/results_screen/results_screen.dart
+++ lib/view/results_screen/results_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_july/dummy_db.dart';
 import 'package:quiz_app_july/utils/color_constants.dart';
 import 'package:quiz_app_july/view/quiz_screen/quiz_screen.dart';
 
-class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+class ResultsScreen extends StatefulWidget {
+  const ResultsScreen({super.key, required this.rightAnsCount});
+  final int rightAnsCount;
+
+  @override
+  State<ResultsScreen> createState() => _ResultsScreenState();
+}
+
+class _ResultsScreenState extends State<ResultsScreen> {
+  int starCount = 0;
+  @override
+  void initState() {
+    claculatePercentage();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +36,35 @@ class ResultsScreen extends StatelessWidget {
                     right: 15, left: 15, bottom: index == 1 ? 30 : 0),
                 child: Icon(
                   Icons.star,
-                  color: Colors.grey,
+                  color: starCount > index ? Colors.yellow : Colors.grey,
                   size: index == 1 ? 70 : 40,
                 ),
               ),
             ),
           ),
+
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Congratulations",
+            style: TextStyle(
+                color: Colors.amber, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Your Score",
+            style: TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "${widget.rightAnsCount}/${DummyDb.questions.length}",
+            style: TextStyle(
+                color: Colors.amber, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+
           //restart button
           SizedBox(height: 40),
           InkWell(
@@ -57,5 +94,17 @@ class ResultsScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  claculatePercentage() {
+    num percentge = (widget.rightAnsCount / DummyDb.questions.length) * 100;
+    if (percentge >= 90) {
+      starCount = 3;
+    } else if (percentge >= 50) {
+      starCount = 2;
+    } else if (percentge >= 30) {
+      starCount = 1;
+    }
+    setState(() {});
   }
 }
